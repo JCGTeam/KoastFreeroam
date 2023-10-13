d3845b9d5d8e9d3562e33ad3c51febd5422b9a2a
Hallkezz <32846474+Hallkezz@users.noreply.github.com>
2023-10-13 21:00:17 +0300

    Add formatNumber function to server-side

--- lua/autorun/client/functions.lua
+++ lua/autorun/client/functions.lua
@@ -4,16 +4,3 @@ function ExtRender:DrawShadowedText( pos, string, color, ShadowColor, size, scal
 	Render:DrawText( pos + Vector2.One, string, ShadowColor, ( size or 16 ), ( scale or 1 ) )
 	Render:DrawText( pos, string, color, ( size or 16 ), ( scale or 1 ) )
 end
-
-function formatNumber( amount )
-	local formatted = tostring( amount )
-
-	while true do  
-		formatted, k = string.gsub( formatted, "^(-?%d+)(%d%d%d)", "%1.%2" )
-		if (k==0) then
-			break
-		end
-	end
-
-	return formatted
-end
